e8b54f82fb6645ac1bef61e210a8208f334a638e
Robrecht Dewaele <rdewaele@infogroep.be>
2012-11-16 21:32:40 +0100

    allow the install script to be called from a random path

--- emacs/cp_to_home.sh
+++ emacs/cp_to_home.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-echo "This script is stupid and will remove your .emacs file and .emacs.d folder"
+# store script location to be able to refer to .emacs(.d) files no matter where
+# the script is called from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "This script is stupid and will remove your .emacs file and .emacs.d folder!"
 read -p "ARE YOU SURE? [y/N] " is_sure
 if (! ( [ "$is_sure" = "y" ] || [ "$is_sure" = "Y" ] ) ); then exit 1; fi
 
 rm -rf ~/.emacs ~/.emacs.d
 
-cp emacs ~/.emacs
-cp -r emacs.d ~/.emacs.d
+cp $DIR/emacs ~/.emacs
+cp -r $DIR/emacs.d ~/.emacs.d
 cd ~/.emacs.d/site-lisp && git clone git://github.com/haskell/haskell-mode.git
