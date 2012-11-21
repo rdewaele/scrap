f655e27b60469c1e98f4799a6133aeff0edabaa2
root <root@ubuntu.(none)>
2012-11-21 11:11:44 +0100

    when 'installing' the emacs config, also install cabal-dev (from their git repo - at the time of writing the hackage version lags behind and is broken with latest ghc versions

--- emacs/cp_to_home.sh
+++ emacs/cp_to_home.sh
@@ -5,8 +5,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "This script is stupid and will remove your .emacs file and .emacs.d folder!"
-read -p "ARE YOU SURE? [y/N] " is_sure
-if (! ( [ "$is_sure" = "y" ] || [ "$is_sure" = "Y" ] ) ); then exit 1; fi
+read -p "ARE YOU SURE? [y/N] " promptanswer
+if (! ( [ "$promptanswer" = "y" ] || [ "$promptanswer" = "Y" ] ) ); then exit 1; fi
 
 rm -rf ~/.emacs ~/.emacs.d
 
@@ -14,9 +14,14 @@ cp $DIR/emacs ~/.emacs
 cp -r $DIR/emacs.d ~/.emacs.d
 cd ~/.emacs.d/site-lisp && git clone https://github.com/haskell/haskell-mode.git
 
+read -p "Would you like to run 'cabal update'? (The next few prompts will ask about installing software from hackage.) [Y/n] " promptanswer
+if (! [ "$promptanswer" = "n" ] || [ "$promptanswer" = "N" ] ); then cabal update; fi
+
 echo "The Haskell mode can use stylish-haskell to indent code."
 echo "Note that this library directly or indirectly depends on the 'happy' package. Please install it prior to continuing with this script."
-read -p "Would you like to install this library through cabal? [Y/n] " cabalrun
-if ( [ "$cabalrun" = "n" ] || [ "$cabalrun" = "N" ] ); then exit 1; fi
-cabal update
-cabal install stylish-haskell
+read -p "Would you like to install this library through cabal? [Y/n] " promptanswer
+if (! [ "$promptanswer" = "n" ] || [ "$promptanswer" = "N" ] ); then cabal install stylish-haskell; fi
+
+echo "The Haskell mode can run 'cabal-dev' instead of just ghci as REPL environment."
+read -p "Would you like to install cabal-dev? [Y/n] "
+if (! [ "$promptanswer" = "n" ] || [ "$promptanswer" = "N" ] ); then cd /tmp && rm -rf cabal-dev && git clone https://github.com/creswick/cabal-dev && cd cabal-dev && cabal configure && cabal build && cabal install; fi
